23c489ec30da9b79b1a327ef6b41fdc441f85c25
iheb1 <mohamed.iheb.khedhri@gmail.com>
2018-09-05 16:10:44 +0100

    image url from app to component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Navigation'; 
+import Facerecong from './facerecong'; 
 import UserImage from './userImage'; 
 import ImageLink from './imageLink'; 
 import Beelogo from './beelogo';
 import BgAnim from './bganim';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: '3b3e4c6fb69140e8a7b44af889f1576e'
+});
 
 
 class App extends Component {
@@ -12,15 +18,24 @@ class App extends Component {
   super();
   this.state={
     input:'',
+    imageUrl:''
   }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input:event.target.value});
  
   }
   onSubmit =(event) =>{
-    console.log('click') ;
+   
+    app.models.predict ( Clarifai.COLOR_MODEL , "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+       console.log(response) ;
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
   render() {
     return (
@@ -32,9 +47,10 @@ class App extends Component {
            <ImageLink 
            onInputChange={this.onInputChange}
            onSubmit={this.onSubmit}
+           imageUrl={this.imageUrl}
             />
            <BgAnim />
-           
+           <Facerecong imageUrl={this.imageUrl}/>
            
            
       </div>
---
+++ src/facerecong.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Facerecong= ({imageUrl}) => {
+    return (
+    	 <div>
+         <img src={imageUrl} alt="image" />
+        </div>
+    	   );
+  
+}
+export default Facerecong ;
